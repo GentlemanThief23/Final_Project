3fc5a77d855f80d19ddf0f2c9f31cdb12762653c
Brandon Green <162610705+GentlemanThief23@users.noreply.github.com>
2024-05-19 07:29:15 -0400

    Random Deletion Difficulty

---
+++ Pickle Journey Final/Assets/Scripts/ChangeLevel.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeLevel : MonoBehaviour
+{
+    int buildindex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        buildindex = SceneManager.GetActiveScene().buildIndex;
+        Debug.Log("build index: " + buildindex);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider myCollision)
+    {
+        SceneManager.LoadScene(buildindex + 1);
+    }
+}
